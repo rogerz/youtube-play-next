512dd2cebbfed40f636afa65b337d4db7dcbc376
Rogerz Zhang <rogerz.zhang@gmail.com>
2011-11-12 15:30:08 +0800

    it works

--- next.js
+++ next.js
@@ -1,23 +1,42 @@
 var Ypn = {
-	titles: {},
-	init: function() {
-        this.current_title = '';
-	},
-	run: function() {
-		this.init();
-        this.current_title = $(#watch-head-line-title).val();
+    next: {},
+    prev: {},
+    title: '',
+    init: function() {
+        this.title = '';
+    },
+    run: function() {
+        this.init();
+        this.title = $('#eow-title').text().trim();
         this.read_items();
-	},
+        this.add_links();
+    },
     read_items: function() {
         var self = this;
-        $('li.video-list-item-link').each(function(idx)) {
-            var title = $(this).attr('title').val();
-            var link = $(this).attr('href').val();
+        $('#watch-related').find('a').filter('.video-list-item-link ').each(function(idx) {
+            var title = $(this).find('[title]').attr('title');
+            var link = $(this).attr('href');
             self.add_item(title, link);
-        }
+        });
     },
     add_item: function(title, link) {
-        this.titles.push({title: title, link: link});
+        if (title < this.title && (!this.prev.title || title > this.prev.title)) {
+            this.prev = {title: title, link: link};
+            console.log('prev: '+title);
+        } else if (title > this.title && (!this.next.title || title < this.next.title)) {
+            this.next = {title: title, link: link};
+            console.log('next: '+title);
+        } else {
+            console.log('skip: '+title);
+        }
     },
-}
+    add_links: function() {
+        if (this.next) {
+            $('#watch-headline-title').append('<a href="'+this.next.link+'" id="next-related-video">'+this.next.title+'</a>');
+        }
+        if (this.prev) {
+            $('#watch-headline-title').prepend('<a href="'+this.prev.link+'"id="prev-related-video">'+this.prev.title+'</a>');
+        }
+    }
+};
 Ypn.run();
